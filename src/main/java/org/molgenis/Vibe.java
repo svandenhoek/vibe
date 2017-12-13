cff9eb8d80db155d68bfd07685fb5cab7bd69735
Sander van den Hoek <savandenhoek@gmail.com>
2017-12-13 14:19:04 +0100

    Reads in rVCF file (and demo code to show reading records using Molgenis Entity objects).

--- src/main/java/org/molgenis/Vibe.java
+++ src/main/java/org/molgenis/Vibe.java
@@ -2,11 +2,15 @@ package org.molgenis;
 
 import org.apache.commons.cli.MissingOptionException;
 import org.apache.commons.cli.ParseException;
+import org.molgenis.data.Entity;
+import org.molgenis.data.annotation.makervcf.structs.VcfEntity;
+import org.molgenis.data.vcf.VcfRepository;
 import org.molgenis.options_digestion.CommandLineOptionsParser;
 import org.molgenis.options_digestion.OptionsParser;
 
 import java.io.IOException;
 import java.nio.file.InvalidPathException;
+import java.util.Iterator;
 
 /**
  * The main application class.
@@ -43,7 +47,17 @@ public class Vibe {
     /**
      * The actual processing parts of the application.
      * @param appOptions {@link OptionsParser}
+     * @throws Exception if {@link VcfEntity#VcfEntity(Entity)} fails
      */
-    public void run(OptionsParser appOptions) throws IOException {
+    public void run(OptionsParser appOptions) throws Exception {
+        // Reads in rVCF for further usage.
+        VcfRepository vcf = new VcfRepository(appOptions.getRvcfData().toFile(), "rvcf");
+
+        // Example of iterating through VcfRepository:
+        Iterator<Entity> vcfIterator = vcf.iterator();
+        while(vcfIterator.hasNext())
+        {
+            VcfEntity record = new VcfEntity(vcfIterator.next());
+        }
     }
 }
