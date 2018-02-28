ae7c7b4ae8317d56dd7cbf5f8b62b3f162e5fc13
Sander van den Hoek <savandenhoek@gmail.com>
2018-02-28 18:59:39 +0100

    Moved knowledge about how to run the app to RunMode.

--- src/main/java/org/molgenis/vibe/VibeApplication.java
+++ src/main/java/org/molgenis/vibe/VibeApplication.java
@@ -1,12 +1,6 @@
 package org.molgenis.vibe;
 
-import org.molgenis.vibe.formats.GeneDiseaseCollection;
-import org.molgenis.vibe.io.ModelReader;
-import org.molgenis.vibe.io.TripleStoreDbReader;
 import org.molgenis.vibe.options_digestion.CommandLineOptionsParser;
-import org.molgenis.vibe.options_digestion.OptionsParser;
-import org.molgenis.vibe.options_digestion.RunMode;
-import org.molgenis.vibe.rdf_processing.GenesForPhenotypeRetriever;
 
 import java.io.IOException;
 
@@ -19,46 +13,18 @@ public class VibeApplication {
      * @param args {@link String}{@code []}
      */
     public static void main(String[] args) {
-        VibeApplication app = new VibeApplication();
-
         try {
             CommandLineOptionsParser appOptions = new CommandLineOptionsParser(args);
-            // If RunMode is NONE, shows help message and quits application.
-            if(appOptions.getRunMode() == RunMode.NONE) {
-                CommandLineOptionsParser.printHelpMessage();
-            } else { // Any other RunMode will continue application.
-                try {
-                    app.run(appOptions);
-                } catch (IOException e) {
-                    System.err.println(e.getLocalizedMessage());
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
+            try {
+                appOptions.getRunMode().run(appOptions);
+            } catch (IOException e) {
+                System.err.println(e.getLocalizedMessage());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         } catch (Exception e) {
             System.err.println(e.getLocalizedMessage());
             CommandLineOptionsParser.printHelpMessage();
-        }
-    }
-
-    /**
-     * The actual processing parts of the application.
-     * @param appOptions {@link OptionsParser}
-     */
-    public void run(OptionsParser appOptions) throws IOException {
-        // Prepares DisGeNET database for querying.
-        appOptions.printVerbose("Preparing DisGeNET dataset");
-        ModelReader modelReader = new TripleStoreDbReader(appOptions.getDisgenetDataDir());
-
-        if(appOptions.getRunMode() == RunMode.GET_GENES_USING_SINGLE_PHENOTYPE) {
-            // Retrieves data from DisGeNET database.
-            appOptions.printVerbose("Retrieving gene-disease associations for given phenotype.");
-            GenesForPhenotypeRetriever genesForPhenotypeRetriever = new GenesForPhenotypeRetriever(modelReader, appOptions.getPhenotypes());
-            genesForPhenotypeRetriever.run();
-
-            // Stores needed data and allows rest to be collected by garbage collector.
-            GeneDiseaseCollection geneDiseaseCollection = genesForPhenotypeRetriever.getGeneDiseaseCollection();
-            genesForPhenotypeRetriever = null;
         }
     }
 }
--- src/main/java/org/molgenis/vibe/options_digestion/RunMode.java
+++ src/main/java/org/molgenis/vibe/options_digestion/RunMode.java
@@ -1,10 +1,36 @@
 package org.molgenis.vibe.options_digestion;
 
+import org.molgenis.vibe.exceptions.CorruptDatabaseException;
+import org.molgenis.vibe.formats.GeneDiseaseCollection;
+import org.molgenis.vibe.io.ModelReader;
+import org.molgenis.vibe.io.TripleStoreDbReader;
+import org.molgenis.vibe.rdf_processing.GenesForPhenotypeRetriever;
+
 /**
  * Describes what the application should do.
  */
 public enum RunMode {
-    NONE("none"), GET_GENES_USING_SINGLE_PHENOTYPE("get genes matching a single phenotype");
+    NONE("none") {
+        @Override
+        public void run(OptionsParser appOptions) {
+            CommandLineOptionsParser.printHelpMessage();
+        }
+    }, GET_GENES_USING_SINGLE_PHENOTYPE("get genes matching a single phenotype") {
+        @Override
+        public void run(OptionsParser appOptions) throws CorruptDatabaseException {
+            appOptions.printVerbose("Preparing DisGeNET dataset.");
+            ModelReader reader = new TripleStoreDbReader(appOptions.getDisgenetDataDir());
+
+            // Retrieves data from DisGeNET database.
+            appOptions.printVerbose("Retrieving gene-disease associations for given phenotype.");
+            GenesForPhenotypeRetriever genesForPhenotypeRetriever = new GenesForPhenotypeRetriever(reader, appOptions.getPhenotypes());
+            genesForPhenotypeRetriever.run();
+
+            // Stores needed data and allows the rest to be collected by garbage collector.
+            GeneDiseaseCollection geneDiseaseCollection = genesForPhenotypeRetriever.getGeneDiseaseCollection();
+            genesForPhenotypeRetriever = null;
+        }
+    };
 
     private String description;
 
@@ -15,6 +41,8 @@ public enum RunMode {
     RunMode(String description) {
         this.description = description;
     }
+
+    public abstract void run(OptionsParser appOptions) throws Exception;
 
     /**
      * Retrieves the {@link RunMode} based on a {@link String} existing out of ONLY numbers.
