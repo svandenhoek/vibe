5a04242b611b8ad5d66af29dd479d4631f1c5b02
Sander van den Hoek <savandenhoek@gmail.com>
2018-07-06 12:58:35 +0200

    Added gene sorters using Disease Specificity Index (DSI) or Disease Pleiotropy Index (DPI).

---
+++ src/main/java/org/molgenis/vibe/query_output_digestion/prioritization/DiseasePleiotropyIndexGenePrioritizer.java
@@ -0,0 +1,23 @@
+package org.molgenis.vibe.query_output_digestion.prioritization;
+
+import org.molgenis.vibe.formats.Gene;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Set;
+
+public class DiseasePleiotropyIndexGenePrioritizer extends GenePrioritizer {
+    public DiseasePleiotropyIndexGenePrioritizer(List<Gene> data) {
+        super(data);
+    }
+
+    public DiseasePleiotropyIndexGenePrioritizer(Set<Gene> data) {
+        super(data);
+    }
+
+    @Override
+    public void run() {
+        // Sorts the genes with lowest disease pleiotropy index first.
+        getPriority().sort(Comparator.comparingDouble(Gene::getDiseasePleiotropyIndex));
+    }
+}
