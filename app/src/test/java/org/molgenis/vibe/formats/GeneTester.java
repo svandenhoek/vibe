bf35ca8ec894c741f818da8128cb6fff6f3cecb1
Sander van den Hoek <savandenhoek@gmail.com>
2019-10-28 14:52:31 +0100

    More tests.

--- app/src/test/java/org/molgenis/vibe/formats/GeneTester.java
+++ app/src/test/java/org/molgenis/vibe/formats/GeneTester.java
@@ -4,19 +4,19 @@ import org.molgenis.vibe.exceptions.InvalidStringFormatException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.net.URI;
+
 public class GeneTester {
     @Test
     public void useValidGeneIdWithPrefix() throws InvalidStringFormatException {
         Gene gene = new Gene("ncbigene:1234");
-        Assert.assertEquals(gene.getId(), "1234");
-        Assert.assertEquals(gene.getFormattedId(), "ncbigene:1234");
+        testValidGene(gene);
     }
 
     @Test
     public void useValidGeneIdWithUpperCasePrefix() throws InvalidStringFormatException {
         Gene gene = new Gene("NCBIGENE:1234");
-        Assert.assertEquals(gene.getId(), "1234");
-        Assert.assertEquals(gene.getFormattedId(), "ncbigene:1234");
+        testValidGene(gene);
     }
 
     @Test(expectedExceptions = InvalidStringFormatException.class)
@@ -42,5 +42,22 @@ public class GeneTester {
     @Test(expectedExceptions = InvalidStringFormatException.class)
     public void useValidGeneIdWithInvalidPrefix() throws InvalidStringFormatException {
         new Gene("ncbi:1234");
+    }
+
+    @Test
+    public void useValidGeneUri() throws InvalidStringFormatException {
+        Gene gene = new Gene(URI.create("http://identifiers.org/ncbigene/1234"));
+        testValidGene(gene);
+    }
+
+    @Test(expectedExceptions = IllegalArgumentException.class)
+    public void useInvalidGeneUri() throws InvalidStringFormatException {
+        Gene gene = new Gene(URI.create("http://identifiers.org/ncbi/1234"));
+    }
+
+    public void testValidGene(Gene gene) {
+        Assert.assertEquals(gene.getId(), "1234");
+        Assert.assertEquals(gene.getFormattedId(), "ncbigene:1234");
+        Assert.assertEquals(gene.getUri(), URI.create("http://identifiers.org/ncbigene/1234"));
     }
 }
