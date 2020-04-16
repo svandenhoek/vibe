878fd44c96fc1dcf55e3fd5537941c3086bdd97b
Sander van den Hoek <savandenhoek@gmail.com>
2020-04-16 13:01:05 +0200

    Updated JavaDoc.

--- app/src/main/java/org/molgenis/vibe/formats/allFieldsEquals.java
+++ app/src/main/java/org/molgenis/vibe/formats/allFieldsEquals.java
@@ -1,5 +1,10 @@
 package org.molgenis.vibe.formats;
 
+/**
+ * A deep equals that compares all fields of a {@link Class}, also ones that are not used by
+ * {@link Class#equals(Object)} (for example due to design-reasons, though validity for all fields still needs to be
+ * checked in unit-tests to assess everything functions as it should).
+ */
 public interface allFieldsEquals {
     boolean allFieldsEquals(Object o);
 }
