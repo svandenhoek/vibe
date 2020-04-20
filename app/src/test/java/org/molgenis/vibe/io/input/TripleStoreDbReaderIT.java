c7065a38fb34b9f50fe7112f4e8f89f1878b4588
Sander van den Hoek <savandenhoek@gmail.com>
2020-04-20 08:38:15 +0200

    Added missing "ExecutionMode.SAME_THREAD" for test class that uses the TDB.

--- app/src/test/java/org/molgenis/vibe/io/input/TripleStoreDbReaderIT.java
+++ app/src/test/java/org/molgenis/vibe/io/input/TripleStoreDbReaderIT.java
@@ -3,10 +3,13 @@ package org.molgenis.vibe.io.input;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.molgenis.vibe.TestData;
 
 import java.io.IOException;
 
+@Execution(ExecutionMode.SAME_THREAD)
 public class TripleStoreDbReaderIT {
     private static String tdbDir;
 
