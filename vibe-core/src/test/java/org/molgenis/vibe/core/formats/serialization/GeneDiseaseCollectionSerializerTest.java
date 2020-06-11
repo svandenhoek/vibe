ff3df9887816c7b7f54c4ff39852561313700a92
Sander van den Hoek <savandenhoek@gmail.com>
2020-06-11 16:24:58 +0200

    Updated/added tests.

--- vibe-core/src/test/java/org/molgenis/vibe/core/formats/serialization/GeneDiseaseCollectionSerializerTest.java
+++ vibe-core/src/test/java/org/molgenis/vibe/core/formats/serialization/GeneDiseaseCollectionSerializerTest.java
@@ -11,36 +11,32 @@ import java.net.URI;
 import java.util.Arrays;
 
 public class GeneDiseaseCollectionSerializerTest {
-    private static GeneDiseaseCombination[] gdcs;
-    private static Gene[] genes;
-    private static Disease[] diseases;
-    private static Source[] sources;
-
+    private static GeneDiseaseCollection geneDiseaseCollection;
     private static Gson gson;
 
     @BeforeAll
     public static void beforeAll() {
-        genes = new Gene[]{
+        Gene[] genes = new Gene[]{
                 new Gene("ncbigene:1111111", new GeneSymbol("hgnc:AAA")),
                 new Gene("ncbigene:2222222", new GeneSymbol("hgnc:BBB"))
         };
 
-        diseases = new Disease[]{
+        Disease[] diseases = new Disease[]{
                 new Disease("umls:C1111111", "a disease name"),
                 new Disease("umls:C2222222", "another disease name"),
                 new Disease("umls:C3333333", "yet another disease")
         };
 
-        gdcs = new GeneDiseaseCombination[]{
+        GeneDiseaseCombination[] gdcs = new GeneDiseaseCombination[]{
                 new GeneDiseaseCombination(genes[0], diseases[0], 0.5), // only counts
                 new GeneDiseaseCombination(genes[0], diseases[1], 0.3), // only counts
                 new GeneDiseaseCombination(genes[1], diseases[1], 0.6), // includes pubmeds
                 new GeneDiseaseCombination(genes[1], diseases[2], 0.3) // only counts
         };
 
-        sources = new Source[]{
-                new Source(URI.create("http://rdf.disgenet.org/v6.0.0/void/ORPHANET"), "Orphanet", Source.Level.CURATED),
-                new Source(URI.create("http://rdf.disgenet.org/v6.0.0/void/BEFREE"), "Befree", Source.Level.LITERATURE)
+        Source[] sources = new Source[]{
+                new Source(URI.create("http://rdf.disgenet.org/v6.0.0/void/ORPHANET"), "Orphanet dataset", Source.Level.CURATED),
+                new Source(URI.create("http://rdf.disgenet.org/v6.0.0/void/BEFREE"), "BeFree dataset", Source.Level.LITERATURE)
         };
 
         // Adds counts.
@@ -54,8 +50,14 @@ public class GeneDiseaseCollectionSerializerTest {
         gdcs[2].add(sources[1], new PubmedEvidence(URI.create("http://identifiers.org/pubmed/3"), 2001));
         gdcs[2].add(sources[1], new PubmedEvidence(URI.create("http://identifiers.org/pubmed/2"), 2001));
 
+        // Generates GeneDiseaseCollection.
+        geneDiseaseCollection = new GeneDiseaseCollection();
+        geneDiseaseCollection.addAll(Arrays.asList(gdcs));
+
+        // Prepares Gson with custom (de)serializer.
         GsonBuilder gsonBuilder = new GsonBuilder().setPrettyPrinting();
         gsonBuilder.registerTypeAdapter(GeneDiseaseCollection.class, new GeneDiseaseCollectionSerializer());
+        gsonBuilder.registerTypeAdapter(GeneDiseaseCollection.class, new GeneDiseaseCollectionDeserializer());
         gson = gsonBuilder.create();
     }
 
@@ -67,9 +69,7 @@ public class GeneDiseaseCollectionSerializerTest {
      */
     @Test
     public void testSerializer() {
-        GeneDiseaseCollection collection = new GeneDiseaseCollection();
-        collection.addAll(Arrays.asList(gdcs));
-        String actualOutput = gson.toJson(collection);
+        String actualOutput = gson.toJson(geneDiseaseCollection);
 
         // ### Json combinations arrays ordering (integer means ordered as integer, not necessarily output is integer) ###
         // gene-disease combinations: gene (integer, ascending), disease (string, ascending)
@@ -109,7 +109,7 @@ public class GeneDiseaseCollectionSerializerTest {
                 "      \"score\": 0.6,\n" +
                 "      \"sources\": [\n" +
                 "        {\n" +
-                "          \"name\": \"Befree\",\n" +
+                "          \"name\": \"BeFree\",\n" +
                 "          \"count\": 3,\n" +
                 "          \"pubmed\": [\n" +
                 "            {\n" +
@@ -167,16 +167,47 @@ public class GeneDiseaseCollectionSerializerTest {
                 "  },\n" +
                 "  \"sources\": {\n" +
                 "    \"Orphanet\": {\n" +
+                "      \"fullName\": \"Orphanet dataset\",\n" +
+                "      \"uri\": \"http://rdf.disgenet.org/v6.0.0/void/ORPHANET\",\n" +
                 "      \"level\": \"curated\"\n" +
                 "    },\n" +
-                "    \"Befree\": {\n" +
+                "    \"BeFree\": {\n" +
+                "      \"fullName\": \"BeFree dataset\",\n" +
+                "      \"uri\": \"http://rdf.disgenet.org/v6.0.0/void/BEFREE\",\n" +
                 "      \"level\": \"literature\"\n" +
                 "    }\n" +
                 "  }\n" +
                 "}";
-
-        System.out.println(actualOutput);
+        
         Assertions.assertEquals(expectedOutput, actualOutput);
+    }
+
+    /**
+     * Goal of custom Gson is to generate less data than if using the default Gson class parser (by eliminating
+     * duplicate data caused by highly the interlinked design).
+     */
+    @Test
+    public void testIfCustomIsSmaller() {
+        // Gson without custom (de)serializer.
+        GsonBuilder gsonBuilder = new GsonBuilder().setPrettyPrinting();
+        Gson defaultJson = gsonBuilder.create();
+
+        String defaultOutput = defaultJson.toJson(geneDiseaseCollection);
+        String customOutput = gson.toJson(geneDiseaseCollection);
+
+        Assertions.assertTrue(customOutput.length() < defaultOutput.length());
+    }
+
+    /**
+     * Validates that when serializing and deserializing a {@link GeneDiseaseCollection},
+     * an identical {@link GeneDiseaseCollection} is generated.
+     */
+    @Test
+    public void testIfObjectIsFullyEqualAfterBeingSerializedAndDeserialized() {
+        String collectionAsJson = gson.toJson(geneDiseaseCollection);
+        GeneDiseaseCollection returnedCollection = gson.fromJson(collectionAsJson, GeneDiseaseCollection.class);
+
+        Assertions.assertTrue(returnedCollection.allFieldsEquals(geneDiseaseCollection));
     }
 
 }
